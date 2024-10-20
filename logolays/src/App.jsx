dd1e6312ed618985176920dd2dc62c4215291bf5
turnermanxi <cturnerelrey@gmail.com>
2024-10-20 00:24:00 -0500

    first commit

---
+++ logolays/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import { Logoscene } from './logo'
+
+function App() {
+
+  return (
+    <>
+      <div className='Canvass'>
+        <Logoscene />
+      </div>
+    </>
+  )
+}
+
+export default App
